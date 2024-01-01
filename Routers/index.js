00094da73d9710e7b24dca09f6249edc067a0626
Yekeen Abiodun <abiodunyekeen76@gmail.com>
2024-01-01 10:15:08 +0000

    update

--- Routers/index.js
+++ Routers/index.js
@@ -8,7 +8,7 @@ const userRoute = require("./user")
 const commentRoute = require("./comment")
 
 router.use("/api/auth",authRoute)
-router.use("/api/story",storyRoute)
+router.use("/story",storyRoute)
 router.use("/api/user",userRoute)
 router.use("/api/comment",commentRoute)
 
