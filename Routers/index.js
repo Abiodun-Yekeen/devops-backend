b8ae84e44e19321ab9dfbdcbcec4a36cb4fbe0b0
Yekeen Abiodun <abiodunyekeen76@gmail.com>
2024-01-01 17:45:57 +0000

    update

--- Routers/index.js
+++ Routers/index.js
@@ -7,10 +7,13 @@ const storyRoute = require("./story")
 const userRoute = require("./user")
 const commentRoute = require("./comment")
 
-router.use("/api/auth",authRoute)
-router.use("/api/story",storyRoute)
-router.use("/api/user",userRoute)
-router.use("/api/comment",commentRoute)
+router.use("/auth",authRoute)
+router.use("/story",storyRoute)
+router.use("/user",userRoute)
+router.use("/comment",commentRoute)
+
+
+
 
 
 module.exports = router
